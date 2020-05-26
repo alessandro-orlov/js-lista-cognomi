6773655863a0c6d35aa6497e57a935b73303387d
Alessandro <alex.ixs@hotmail.com>
2020-05-26 19:36:48 +0200

    stato intermedio

--- js/myscript.js
+++ js/myscript.js
@@ -1,26 +1,32 @@
 //Surname list (Arrey)
 var surnameList = ["Bianchi", "Rossi", "Duzioni", "Balsano", "Verdi"];
 
-//List Element
-var elementList = document.getElementById('surname-list');
-
 //Add surname Form
-var addSurname = document.getElementById('add-surname');
-
-//Event button
+var userSurname = document.getElementById('user-surname');
 var generateListBtn = document.getElementById('generate-list');
 
-//Add Surname to the list
-generateListBtn.addEventListener('click',
+//List Element - Lista invitati
+var elementList = document.getElementById('surname-list');
+
+for(var i = 0; i < surnameList.length; i++) {
+  var fullList = elementList.innerHTML;
+  elementList.innerHTML = fullList + '<li>' + surnameList[i] + '</li>'
+}
+
+//Add user surname to the list
+generateListBtn.addEventListener( 'click',
   function() {
-    //add user surname to the existing list
-    surnameList.push(addSurname.value);
+    //Error se il campo è vuoto
+    if(userSurname.value == '') {
+      alert('Non hai inserito il cognome');
+    } else {
+      //aggiunge
+      surnameList.push(userSurname.value);
 
-    var alphabeticalOrderList = surnameList.sort();
-
-    for (var i = 0; i < alphabeticalOrderList.length; i++) {
-      var contenutoCorrente = elementList.innerHTML;
-      elementList.innerHTML = contenutoCorrente + '<li>' + alphabeticalOrderList[i] + '</li>';
+    for(var i = 0; i < surnameList.length; i++) {
+      var fullList = elementList.innerHTML;
+      elementList.innerHTML = fullList + '<li>' + surnameList[i] + '</li>'
+      }
     }
   }
-)
+);
