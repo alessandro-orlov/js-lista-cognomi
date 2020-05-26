161bb5807adcb61d79ad9784da0842c071b0a2bc
Alessandro <alex.ixs@hotmail.com>
2020-05-26 14:25:13 +0200

    add surname correctly with element.push()

--- js/myscript.js
+++ js/myscript.js
@@ -1,8 +1,14 @@
+//Lista di cognomi (Arrey)
+var listaCognomi = ["Bianchi", "Rossi", "Duzioni", "Balsano", "Verdi"];
+
 // Chidere cognome all'utente
+var cognomeUtente = prompt('Quale il tuo cognome ?');
 
 //Inserirlo nella lista
+listaCognomi.push(cognomeUtente);
 
-//Lista di cognomi (Arrey)
-
+for(var i = 0; i < listaCognomi.length; i++) {
+  console.log(listaCognomi[i]);
+}
 
 //Output
