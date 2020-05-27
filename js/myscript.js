5a1b6c12f10bb57674ebdb942230b54df35f996e
Alessandro <alex.ixs@hotmail.com>
2020-05-27 09:57:56 +0200

    Exercise finally works as supposed

--- js/myscript.js
+++ js/myscript.js
@@ -7,11 +7,12 @@ var generateListBtn = document.getElementById('generate-list');
 
 //List Element - Lista invitati
 var elementList = document.getElementById('surname-list');
+var orderSurnameBtn = document.getElementById('order-surname-btn');
 
-// for(var i = 0; i < surnameList.length; i++) {
-//   var fullList = elementList.innerHTML;
-//   elementList.innerHTML = fullList + '<li>' + surnameList[i] + '</li>'
-// }
+for(var i = 0; i < surnameList.length; i++) {
+  var fullList = elementList.innerHTML;
+  elementList.innerHTML = fullList + '<li>' + surnameList[i] + '</li>'
+}
 
 //Add user surname to the list
 generateListBtn.addEventListener( 'click',
@@ -24,40 +25,35 @@ generateListBtn.addEventListener( 'click',
       //Add user Surname to the Arrey
       surnameList.push(userSurname.value);
 
-      surnameList.sort(function (a, b) {
-        return a.toLowerCase().localeCompare(b.toLowerCase());
-        }); // si è stackverflkow!
 
-      // // Add user's surname to the existing list
-      // for(var i = 0; i < surnameList.length; i++) {
-      // var fullList = elementList.innerHTML;
-      // }
-      // elementList.innerHTML = fullList + '<li>' + userSurname.value + '</li>'
-
-      //Add whole list in alphabetical order
+      // Add user's surname to the existing list
       for(var i = 0; i < surnameList.length; i++) {
-        var fullList = elementList.innerHTML;
-        elementList.innerHTML = fullList + '<li>' + surnameList[i] + '</li>'
+      var fullList = elementList.innerHTML;
       }
-
-      var surnameNumber = document.getElementById('surname_position');
-      surnameNumber.innerHTML = "Il tuo cognome si trova alla posizione numero # " + (surnameList.indexOf(userSurname.value)+1);
+      elementList.innerHTML = fullList + '<li>' + userSurname.value + '</li>'
 
       console.log(surnameList)
     }
   }
 );
 
-// var orderSurnameBtn = document.getElementById('order-surname-btn');
-//
-// orderSurnameBtn.addEventListener('click',
-//   function() {
-//     surnameList.sort();
-//     console.log(surnameList)
-//
-//    for(var i = 0; i < surnameList.length; i++) {
-//    var fullList = elementList.innerHTML;
-//    elementList.innerHTML = fullList + '<li>' + surnameList[i] + '</li>'
-//      }
-//   }
-// );
+// Order list in alphabetical order
+orderSurnameBtn.addEventListener('click',
+  function() {
+
+    surnameList.sort(function (a, b) {
+    return a.toLowerCase().localeCompare(b.toLowerCase());
+    }); // si è stackverflkow!
+
+    //Reset first list
+    elementList.innerHTML = '';
+
+    for(var i = 0; i < surnameList.length; i++) {
+    var fullList = elementList.innerHTML;
+    elementList.innerHTML = fullList + '<li>' + surnameList[i] + '</li>'
+     }
+
+    var surnameNumber = document.getElementById('surname_position');
+    surnameNumber.innerHTML = "Il tuo cognome si trova alla posizione numero # " + (surnameList.indexOf(userSurname.value)+1);
+  }
+);
